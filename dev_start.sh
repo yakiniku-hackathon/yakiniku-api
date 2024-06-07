fe7e83a96cb4dc632ce3325cd937545fc78d5264
CoCo9122 <banaji.yu0517@outlook.jp>
2024-06-07 23:54:23 +0900

    少し修正

--- dev_start.sh
+++ dev_start.sh
@@ -1,9 +1,2 @@
 #!/bin/bash
-
-sleep 60
-
-python manage.py migrate
-
-source load_all_fixtures.sh
-
-python manage.py runserver 0.0.0.0:8000
+sleep 10 && python manage.py migrate && source load_all_fixtures.sh && python manage.py runserver 0.0.0.0:8000
