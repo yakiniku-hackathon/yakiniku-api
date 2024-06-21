332a054651b7d8c6ba48c6349f997e1c99bf4197
CoCo9122 <banaji.yu0517@outlook.jp>
2024-06-21 17:52:23 +0900

    新規APIの追加

--- mystery/urls.py
+++ mystery/urls.py
@@ -1,6 +1,14 @@
+from django.urls import path
 
-# Import user views here.
+from .views import get_mysteries, get_mystery, get_question, get_questions, patch_mystery, patch_question, post_mystery, post_question
 
 urlpatterns = [
-
+    path("mystery", post_mystery, name="post_mystery"),
+    path("mystery/", get_mysteries, name="get_mysteries"),
+    path("mystery/<int:mystery_id>", patch_mystery, name="patch_mystery"),
+    path("mystery/<int:mystery_id>/", get_mystery, name="get_mystery"),
+    path("mystery/<int:mystery_id>/question", post_question, name="post_question"),
+    path("mystery/<int:mystery_id>/question/", get_questions, name="get_questions"),
+    path("mystery/<int:mystery_id>/question/<int:question_id>", patch_question, name="patch_question"),
+    path("mystery/<int:mystery_id>/question/<int:question_id>/", get_question, name="get_question"),
 ]
